ea4fa144fa95c0a1e8c323e66e4d0b37acc67197
mario2230 <124641210+mario2230@users.noreply.github.com>
2026-03-24 16:02:47 -0300

    refactpr: usar useTarefas e CardTarefa na TarefasPage

--- src/components/CardTarefa.vue
+++ src/components/CardTarefa.vue
@@ -1,4 +1,12 @@
 <script setup lang='ts'>
+import {
+  IonCard,
+  IonCardContent,
+  IonItem,
+  IonIcon,
+  IonLabel,
+  IonButton
+} from '@ionic/vue'
 import { checkmarkCircle, ellipseOutline, trashOutline } from 'ionicons/icons'
 interface Tarefa { id: number; texto: string; feita: boolean }
 
